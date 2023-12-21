b8bcfd22c4dc2050d465fad15af186fc0bf4e77b
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-21 15:57:26 -0500

    - Fix styling issue with Works page
    - Add discord-landing-page.gif  for Works.jsx page
    - Add under-construction-img.jpg  for Works.jsx page
    - Change Discover button to view live demo
    - Change Explore Code button to View Code
    - Fix project summary p tag to have media query
    - Add media query to Work.tsx section width
    - Change BGPattern to be outside of Work.tsx section
    - Add Github links to all current projects
    - Change buttons for each project to a tags
    - Add swiper npm package for  Works.tsx

--- src/components/BGPattern.jsx
+++ src/components/BGPattern.jsx
@@ -3,8 +3,8 @@ const BGPattern = () => {
 		<div
 			className="pointer-events-none fixed -z-[5]
       h-full w-screen
-			bg-[radial-gradient(circle,_#cccccc_4%,_transparent_11%),radial-gradient(circle_at_bottom_left,_#cccccc_5%,_transparent_6%),radial-gradient(circle_at_bottom_right,_#cccccc_5%,_transparent_6%),radial-gradient(circle_at_top_left,_#cccccc_5%,_transparent_6%),radial-gradient(circle_at_top_right,_#cccccc_5%,_transparent_6%)]
-       dark:bg-[radial-gradient(circle,_#465373_10%,_transparent_11%),radial-gradient(circle_at_bottom_left,_#465373_5%,_transparent_6%),radial-gradient(circle_at_bottom_right,_#465373_5%,_transparent_6%),radial-gradient(circle_at_top_left,_#465373_5%,_transparent_6%),radial-gradient(circle_at_top_right,_#465373_5%,_transparent_6%)] dark:bg-[#01183f] opacity-80 [background-size:1rem_1rem]"></div>
+			bg-[radial-gradient(circle,_#cccccc_4%,_transparent_4%),radial-gradient(circle_at_bottom_left,_#cccccc_5%,_transparent_6%),radial-gradient(circle_at_bottom_right,_#cccccc_5%,_transparent_4%),radial-gradient(circle_at_top_left,_#cccccc_5%,_transparent_6%),radial-gradient(circle_at_top_right,_#cccccc_5%,_transparent_4%)]
+       dark:bg-[radial-gradient(circle,_#465373_4%,_transparent_11%),radial-gradient(circle_at_bottom_left,_#465373_5%,_transparent_6%),radial-gradient(circle_at_bottom_right,_#465373_5%,_transparent_6%),radial-gradient(circle_at_top_left,_#465373_5%,_transparent_6%),radial-gradient(circle_at_top_right,_#465373_5%,_transparent_6%)] dark:bg-[#01183f] opacity-80 [background-size:1rem_1rem]"></div>
 	);
 };
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,15 +2,87 @@ import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import BGPattern from "./BGPattern";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import discordGIF from "../assets/giphy-discord-landing-page.gif";
+import underConstructionIMG from "../assets/under-construction-img.jpg";
 
 const Work = () => {
+	const projects = [
+		{
+			name: "CineEquip",
+			summary: "Full-stack software application using MERN (MongoDB, Express.js, React.js, Node.js + Typescript) designed to address the organizational needs of professionals in the film industry, offering a solution to enhance the management of project gear.",
+			techUsed: ["React", "Tailwind", "Zustand", "Express", "Mongoose"],
+			githubLink: "https://github.com/joao-miguel8/cineEquip",
+			img: underConstructionIMG,
+		},
+		{ name: "Portfolio", summary: "my portfolio website showcasing my current skills and my collection of portfolio projects I have worked on.", techUsed: ["React", "Tailwind"], githubLink: "https://github.com/joao-miguel8/portfolio-website", img: "" },
+
+		{
+			name: "Discord Landing Page",
+			summary: "A no framework landing page showcasing animated transitions and responsive design principles",
+			techUsed: ["HTML", "CSS", "Javascript"],
+			githubLink: "https://github.com/joao-miguel8/discord-landing-page",
+			img: discordGIF,
+		},
+	];
+
 	return (
 		<>
-			<section>
-				<BGPattern />
+			<BGPattern />
+			<section className="mx-auto w-full lg:w-[60rem] h-full">
 				<Header />
-				<div className="mx-4">
+				<div>
 					<Navbar />
+					<motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
+						<Swiper
+							className="w-full mt-10 h-full"
+							modules={[Pagination]}
+							pagination={{
+								clickable: true,
+							}}
+							spaceBetween={50}>
+							<div>
+								{projects.map(project => {
+									return (
+										<SwiperSlide>
+											<h1 className="text-34 dark:text-white montserrat">{project.name}</h1>
+											<p className="mt-10 w-full md:w-[40rem] text-20 dark:text-white montserrat">{project.summary}</p>
+											<div className="mt-10 flex items-center flex-wrap gap-2 text-bold dark:text-white">
+												<h4 className="text-16  montserrat ">Skills Used:</h4>
+												{project.techUsed.map((tech, index) => {
+													return (
+														<>
+															<h5 className="font-bold robotoMono">{tech}</h5>
+															{index < project.techUsed.length - 1 && <span className="w-2 h-2 rounded-full bg-[#43baea]"></span>}
+														</>
+													);
+												})}
+											</div>
+											<div className="mt-8 flex gap-4 flex-wrap">
+												<a
+													href={project.githubLink}
+													target="_blank"
+													className="font-bold text-[14px] text-[#fff] bg-[#43baea] px-[22px] py-[6px] border-[solid] border-[#43baea] border-[2px] [box-shadow:rgb(0,_0,_0)_0px_0px_0px_0px] rounded-[50px] [transition:621ms] translate-y-[0] flex flex-row items-center cursor-pointer hover:[transition:800ms] hover:-translate-y-0 hover:bg-[#fff] hover:text-[#43baea] hover:border-[solid] hover:border-[2px] hover:border-[#43baea] montserrat">
+													View live demo
+												</a>
+												<a
+													href="#"
+													className="px-4 py-2 text-[14px] font-bold border border-black dark:border-white hover:bg-black hover:text-white dark:hover:text-white dark:hover:border-black hover:dark:text-white dark:text-white
+												transition ease-in-out duration-500 [box-shadow:0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-filter backdrop-blur-[8.1px] border-[solid] montserrat">
+													View Code
+												</a>
+											</div>
+											<img src={project.img} alt={`${project.name} image`} className=" mt-4 w-[400px] border-2 h-60" />
+										</SwiperSlide>
+									);
+								})}
+							</div>
+						</Swiper>
+					</motion.div>
 				</div>
 			</section>
 		</>
