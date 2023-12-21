979b0d356d827be43411a09edff6e78d23eb08b9
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-12-21 16:28:51 -0500

    - Add media query for Works section - Remove email section from Contact page - Change github and Linkedin icon size - Remove unncessary div in discord and linkedin buttons - Change label from wrapping textArea

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,41 +10,43 @@ const Contact = () => {
 	const [descriptionInput, setDescriptionInput] = useState("");
 
 	return (
-		<section>
+		<>
 			<BGPattern />
-			<Header />
-			<div className="mx-4">
-				<NavBar />
-				<motion.div animate={{ x: 80, y: 0 }} className="mt-6 relative -left-20">
-					<h3 className="text-center text-32 dark:text-[#1fb3ed]">Want to work with me?</h3>
-					<form className="flex flex-col gap-4" action="https://api.web3forms.com/submit" method="POST">
-						{/* connect form to web3forms endpoint with public api key */}
-						<input type="hidden" name="apikey" value="15e1c0c6-fe6c-4ab2-a32d-ed52e7a3a762"></input>
-						{/* Name input */}
-						<label required htmlFor="name" className=" flex flex-col dark:text-white robotoMono">
-							Name
-							<input
-								required
-								type="text"
-								name="name"
-								onChange={e => setNameInput(e.target.value)}
-								className={classNames("pb-1 bg-transparent border-b-[1px] border-black dark:border-white focus:outline-none focus:border-[#1fb3ed] focus:text-[#49c2f1] dark:focus:border-[#1fb3ed] transition ease-in-out duration-300")}
-							/>
-						</label>
-						{/* Email Input */}
-						<label htmlFor="email" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" className=" flex flex-col dark:text-white robotoMono">
-							E-mail
-							<input
-								required
-								onChange={e => setEmailInput(e.target.value)}
-								type="email"
-								name="email"
-								className="pb-1 bg-transparent border-b-[1px] border-black dark:border-white focus:outline-none focus:border-[#1fb3ed] focus:text-[#49c2f1] dark:focus:border-[#1fb3ed] transition ease-in-out duration-300"
-							/>
-						</label>
-						{/* description textArea */}
-						<label htmlFor="description" onChange={e => setDescriptionInput(e.target.value)} className="flex flex-col mt-4 dark:text-white robotoMono">
-							Send a message
+			<section className="mb-4 pt-10 mx-auto w-full lg:w-[60rem] h-full">
+				<Header />
+				<div className="mx-4">
+					<NavBar />
+					<motion.div animate={{ x: 80, y: 0 }} className="mt-6 relative -left-20">
+						<h3 className="text-center text-32 dark:text-[#1fb3ed]">Want to work with me?</h3>
+						<form className="flex flex-col gap-4" action="https://api.web3forms.com/submit" method="POST">
+							{/* connect form to web3forms endpoint with public api key */}
+							<input type="hidden" name="apikey" value="15e1c0c6-fe6c-4ab2-a32d-ed52e7a3a762"></input>
+							{/* Name input */}
+							<label required htmlFor="name" className=" flex flex-col dark:text-white robotoMono">
+								Name
+								<input
+									required
+									type="text"
+									name="name"
+									onChange={e => setNameInput(e.target.value)}
+									className={classNames("pb-1 bg-transparent border-b-[1px] border-black dark:border-white focus:outline-none focus:border-[#1fb3ed] focus:text-[#49c2f1] dark:focus:border-[#1fb3ed] transition ease-in-out duration-300")}
+								/>
+							</label>
+							{/* Email Input */}
+							<label htmlFor="email" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" className=" flex flex-col dark:text-white robotoMono">
+								E-mail
+								<input
+									required
+									onChange={e => setEmailInput(e.target.value)}
+									type="email"
+									name="email"
+									className="pb-1 bg-transparent border-b-[1px] border-black dark:border-white focus:outline-none focus:border-[#1fb3ed] focus:text-[#49c2f1] dark:focus:border-[#1fb3ed] transition ease-in-out duration-300"
+								/>
+							</label>
+							{/* description textArea */}
+							<label htmlFor="description" onChange={e => setDescriptionInput(e.target.value)} className="flex flex-col mt-4 dark:text-white robotoMono">
+								Send a message
+							</label>
 							<textarea
 								required
 								rows={5}
@@ -52,32 +54,19 @@ const Contact = () => {
 								name="description"
 								className="mt-1 p-1 resize-none bg-transparent border-[1px] border-black dark:border-white focus:outline-none focus:border-[#1fb3ed] focus:text-[#49c2f1] dark:focus:border-[#1fb3ed] transition ease-in-out duration-300"
 							/>
-						</label>
-						<input type="hidden" name="redirect" value="https://web3forms.com/success" />
-						<motion.button
-							whileHover={{ scale: 1.05 }}
-							type="submit"
-							className="p-2 my-6 mx-auto w-5/6 text-16 max-[300px]:text-12 border text-black bg-white border-neutral-300 hover:text-white hover:bg-#1fb3ed] dark:hover:bg-[#1fb3ed] hover:bg-[#1fb3ed] robotoMono shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] hover:transition ease-in-out hover:duration-300	">
-							Get In Touch
-						</motion.button>
-					</form>
-					{/* External media links */}
-					<div className="pb-8 flex flex-col items-center">
-						<div className="flex gap-2 max-[280px]:flex-col max-[280px]:items-center">
-							<span className="span font-bold text-12 text-black dark:text-white montserrat">E-MAIL</span>
-							{/* email link */}
-							<a
-								href="#"
-								pattern="^((https?|ftp)\://((\[?(\d{1,3}\.){3}\d{1,3}\]?)|(([-a-zA-Z0-9]+\.)+[a-zA-Z]{2,4}))(\:\d+)?(/[-a-zA-Z0-9._?,'+&amp;%$#=~\\]+)*/?)$"
-								className="text-12 text-[#1fb3ed] hover:text-[#1283b0] dark:text-[##1fb3ed] dark:hover:text-[#1283b0] robotoMono transition ease-in-out duration-300">
-								blahblahblah@gmail.com
-							</a>
-						</div>
-
-						<div className="mt-2 right-10 flex gap-2 items-center align-bottom ">
+							<input type="hidden" name="redirect" value="https://web3forms.com/success" />
+							<motion.button
+								whileHover={{ scale: 1.05 }}
+								type="submit"
+								className="p-2 my-6 mx-auto w-5/6 text-16 max-[300px]:text-12 border text-black bg-white border-neutral-300 hover:text-white hover:bg-#1fb3ed] dark:hover:bg-[#1fb3ed] hover:bg-[#1fb3ed] robotoMono shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] hover:transition ease-in-out hover:duration-300	">
+								Get In Touch
+							</motion.button>
+						</form>
+						{/* github / linkedin external media links */}
+						<div aria-label="media links section" className="mt-2 right-10 flex gap-4 items-center justify-center align-bottom ">
 							{/* Github link */}
-							<a href="https://github.com/joao-miguel8" target="_blank">
-								<svg className="w-5 h-5 fill-[#1A96C6] hover:fill-[#0d688c] transition ease-in-out duration-300" id="Layer_1" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<a aria-label="external link to github" href="https://github.com/joao-miguel8" target="_blank">
+								<svg className="w-10 h-10 fill-[#1A96C6] hover:fill-[#0d688c] transition ease-in-out duration-300" id="Layer_1" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 									<g>
 										<path
 											clip-rule="evenodd"
@@ -88,17 +77,17 @@ const Contact = () => {
 								</svg>
 							</a>
 							{/* Linkedin Link */}
-							<a href="https://www.linkedin.com/in/jo%C3%A3omigueltech/" target="_blank">
-								<svg className="w-5 h-5 fill-[#1A96C6] hover:fill-[#0d688c] transition ease-in-out duration-300 " id="linkedin" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+							<a aria-label="external link to linkedin" href="https://www.linkedin.com/in/jo%C3%A3omigueltech/" target="_blank">
+								<svg className="w-10 h-10 fill-[#1A96C6] hover:fill-[#0d688c] transition ease-in-out duration-300 " id="linkedin" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
 									<title />
 									<path d="M454.26,23.47H57.74A34.21,34.21,0,0,0,23.63,57.58V454.1a34.21,34.21,0,0,0,34.11,34.11H454.26a34.21,34.21,0,0,0,34.11-34.11V57.58A34.21,34.21,0,0,0,454.26,23.47Zm-290.4,396H95.39V198.13h68.47ZM129.63,167.09a40.55,40.55,0,1,1,40.55-40.55A40.54,40.54,0,0,1,129.63,167.09ZM422.92,419.52H354V298.1s1.61-24.34-11.41-37c-11.84-11.49-28.3-8.68-28.3-8.68s-18.31.46-28.3,16c-6,9.33-7.76,22.05-8.21,30.7V419.52H209.3V198.13h68.47v26.66c6.2-8.81,17.07-20.54,32.86-26.66,24-9.28,44.61-7.92,62.32-2.58,22.21,6.69,34.88,25.36,39.47,34.54,6.17,12.32,10.5,28.75,10.5,49.3Z" />
 								</svg>
 							</a>
 						</div>
-					</div>
-				</motion.div>
-			</div>
-		</section>
+					</motion.div>
+				</div>
+			</section>
+		</>
 	);
 };
 
