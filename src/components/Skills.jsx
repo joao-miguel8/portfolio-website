86eee915d57c4a83f5fdb95a49d6e8124ea228c5
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-02-02 14:13:19 -0500

    - Change skills data structure and change styling of skills list
    - Change skills styling to use grid
    - Add react-icons npm package
    - Add icons for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,27 @@
+import { FaReact, FaCss3Alt, FaHtml5, FaSass, FaNodeJs, FaGitAlt } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
+import { SiTailwindcss, SiTypescript, SiJavascript } from "react-icons/si";
+import { IoLogoFirebase } from "react-icons/io5";
+
 import { motion } from "framer-motion";
 import NavBar from "./Navbar";
 import Header from "./Header";
 import BGPattern from "./BGPattern";
-
+const size = "26px";
 const Skills = () => {
-	const skills = ["React", "HTML", "CSS", "SASS", "Tailwind", "NODE", "Javascript", "Git", "Firebase"];
+	const skills = [
+		{ skill: "REACT", logo: <FaReact color="#1A95c6" size={size} /> },
+		{ skill: "HTML", logo: <FaHtml5 color="#F06529" size={size} /> },
+		{ skill: "CSS", logo: <FaCss3Alt color="#1A95c6" size={size} /> },
+		{ skill: "SASS", logo: <FaSass color="#c69" size={size} /> },
+		{ skill: "TAILWIND", logo: <SiTailwindcss color="#1A95c6" size={size} /> },
+		{ skill: "NODE", logo: <FaNodeJs color="#68A063" size={size} /> },
+		{ skill: "JAVASCRIPT", logo: <SiJavascript color="#F0DB4F" size={size} /> },
+		{ skill: "TYPESCRIPT", logo: <SiTypescript color="#1A95c6" size={size} /> },
+		{ skill: "GIT", logo: <FaGitAlt color="#f15025" size={size} /> },
+		{ skill: "FIREBASE", logo: <IoLogoFirebase color="#f15025" size={size} /> },
+		{ skill: "MONGODB", logo: <DiMongodb color="#4DB33D" size={size} /> },
+	];
 
 	const variants = {
 		hidden: { opacity: 0 },
@@ -36,13 +53,14 @@ const Skills = () => {
 				<Header />
 				<div className="mx-4">
 					<NavBar />
-					<div className="mt-6">
-						<p className="text-black dark:text-white font-regular montserrat">Here is a list of all my skills, this list is steadily growing over time.</p>
-						<motion.ul variants={variants} initial="hidden" animate="show" className="mt-4 flex flex-col gap-2">
+					<div className="mt-20">
+						<p className="text-center text-20 font-bold text-black dark:text-white font-regular montserrat">A collection of skills I have worked with.</p>
+						<motion.ul variants={variants} initial="hidden" animate="show" className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
 							{skills.map((skill, i) => {
 								return (
-									<motion.li variants={item} key={i} className="text-20 text-[#1783ae] dark:text-[#20b6f1] robotoMono">
-										{skill}
+									<motion.li variants={item} key={i} className="p-2 flex items-center justify-center gap-4 text-18 font-bold text-[#1783ae] dark:text-white robotoMono bg-white/20 shadow-lg ">
+										{skill.logo}
+										<span>{skill.skill}</span>
 									</motion.li>
 								);
 							})}
