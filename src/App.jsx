155147a6bc338bf31fddddc36de069e6eb7d7f4c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-05-27 00:36:36 -0400

    - Rename About.jsx to Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavBar from "./components/Navbar";
-import About from "./components/About";
+import About from "./components/Hero.jsx";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Work from "./components/Works.jsx";
