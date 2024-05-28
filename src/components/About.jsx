0a8d31c24945da38497041c097275d17f344558a
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-05-28 11:02:03 -0400

    - Change Skills.jsx to About.jsx and change styling and content

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+	return (
+		<>
+			<section className="mx-14 mt-40 lg:w-[60rem] h-full">
+				<h2 className="text-34 uppercase font-semibold">A little about me.</h2>
+				<p className="mt-4 text-18 leading-8">
+					My journey began in the world of software development, initially fueled by a passion for game development. As I went deeper, I found myself learning web/software development as a hobby and gaining some freelance experience. It wasn't long before I discovered the dynamic field of
+					digital marketing. I realized that even the most brilliant website or SaaS product can't reach its full potential without effective marketing. This led me down the path of SEO and PPC.
+				</p>
+			</section>
+		</>
+	);
+};
+
+export default About;
