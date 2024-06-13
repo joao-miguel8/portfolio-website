754c1fe8ea973fc82aa79a7ca59f8374c4813fda
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-06-13 11:36:10 -0400

    - delete cloud-icon and add image directly

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,7 +21,10 @@ const HeroSection = ({ scrollToForm }) => {
 							</a>
 							{/* Resume */}
 							<a download href="public/dgm_resume_2024.pdf" className="p-1 flex flex-col items-center shadow-md rounded-lg [box-shadow:rgba(9,_30,_66,_0.25)_0px_4px_8px_-2px,_rgba(9,_30,_66,_0.08)_0px_0px_0px_1px] transition duration-300 ease-in-out hover:-translate-y-2">
-								<img src="public/cloud-icon.svg" alt="download icon" className="w-6" />
+								<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24" alt="download icon">
+									<path d="M18.348,7.23c-1.311-3.151-4.395-5.23-7.848-5.23C5.813,2,2,5.813,2,10.5c0,.551,.053,1.097,.157,1.633-1.347,1.032-2.157,2.646-2.157,4.367,0,3.032,2.243,5.5,5,5.5h11.5c4.136,0,7.5-3.364,7.5-7.5,0-3.467-2.344-6.437-5.652-7.27Zm-4.934,10.184c-.39,.39-.902,.585-1.414,.585s-1.024-.195-1.414-.585l-3.293-3.293,1.414-1.414,2.293,2.293v-6h2v6l2.293-2.293,1.414,1.414-3.293,3.293Z" />
+								</svg>
+
 								<span className="text-12 font-semibold uppercase">Resume</span>
 							</a>
 						</div>
