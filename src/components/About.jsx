b0297ed8c30b2a22afe4d9e18f276f102f72bc60
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-24 10:58:49 -0400

    - Change text styling and font

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ const About = () => {
 	return (
 		<>
 			<section id="about" className="mx-14 pt-40 mt-20 lg:w-[60rem] min-h-[200px]">
-				<h2 className="text-34 uppercase font-semibold">A little about me.</h2>
-				<p className="mt-4 text-18 leading-8">
+				<h2 className="text-34 monoSlab font-semibold">Hi, I'm João!</h2>
+				<p className="mt-4 text-18 leading-8 montserrat">
 					My journey began in the world of software development, initially fueled by a passion for game development. As I went deeper, I found myself learning web/software development as a hobby and gaining some freelance experience. It wasn't long before I discovered the dynamic field of
 					digital marketing. I realized that even the most brilliant website or SaaS product can't reach its full potential without effective marketing. This led me down the path of SEO and PPC.
 				</p>
