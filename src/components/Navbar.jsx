35febb82c33b66232d3d64340d67a39a6fbfa0aa
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-24 12:57:08 -0400

    - Add logo to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,19 +12,22 @@ const NavBar = () => {
 	];
 
 	return (
-		<nav className="mt-8 mb-20 p-4 sticky top-0 z-50 flex justify-center flex-wrap gap-4 bg-white border-b-2 border-b-gray-100">
-			<ul className="flex gap-6">
-				{navLinks.map(navLink => {
-					return (
-						<a key={navLink.to} href={navLink.link} className="cursor-pointer">
-							<li className={classNames(themeMode.isDarkTheme ? "text-[#333]" : "dark:text-[#333]", "font-bold text-18 robotoMono hover:text-green-500")} key={navLink.to}>
-								{navLink.name}
-							</li>
-						</a>
-					);
-				})}
-			</ul>
-		</nav>
+		<header className="px-2 mb-20 py-4 flex items-center border-b-2 border-b-gray-100">
+			<img src="src/assets/favicon.png" alt="" className="w-10" />
+			<nav className="mx-auto p-4 sticky top-0 z-50 flex justify-center flex-wrap gap-4 bg-white">
+				<ul className="flex gap-6">
+					{navLinks.map(navLink => {
+						return (
+							<a key={navLink.to} href={navLink.link} className="cursor-pointer">
+								<li className={classNames(themeMode.isDarkTheme ? "text-[#333]" : "dark:text-[#333]", "font-bold text-18 robotoMono hover:text-green-500")} key={navLink.to}>
+									{navLink.name}
+								</li>
+							</a>
+						);
+					})}
+				</ul>
+			</nav>
+		</header>
 	);
 };
 
