6d16d3727cb133a141cc166575db89fa4ba21c73
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-25 17:43:22 -0400

    - Change the Content layout and Core Services Text Styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
 		<>
 			<section id="about" className="mx-14 pt-40 mt-20 min-h-[200px]">
 				<h2 className="text-34 monoSlab font-semibold">Hi, I'm João!</h2>
+				<p className="mt-4 text-18 leading-8 montserrat">My journey began in the world of software development, initially fueled by a passion for game development. As I went deeper, I've learned web/software development in my free time and gained some freelance work experience.</p>
 				<p className="mt-4 text-18 leading-8 montserrat">
-					My journey began in the world of software development, initially fueled by a passion for game development. As I went deeper, I found myself learning web/software development as a hobby and gaining some freelance experience. It wasn't long before I discovered the dynamic field of
-					digital marketing. I realized that even the most brilliant website or SaaS product can't reach its full potential without effective marketing. This led me down the path of all things digital marketing.
+					It wasn't long before I discovered the dynamic field of digital marketing. I realized that even the most brilliant website or SaaS product can't reach its full potential without effective marketing. This led me down the path of all things digital marketing.
 				</p>
-				<h3 className="mt-10 text-30">Core Services</h3>
+				<h3 className="mt-20 text-24 font-semibold uppercase">Core Services</h3>
 				<ul className="mt-4 flex flex-col gap-4 md:flex-row">
 					{coreServices.map(service => {
 						return (
