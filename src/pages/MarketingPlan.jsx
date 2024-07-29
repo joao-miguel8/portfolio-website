3d0d31cf7d386bfbaf3210ccb74ff1a14a3f3711
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-29 17:47:15 -0400

    - Rename PdfGuideModal.jsx to MarketingPlan.jsx and add it to Pages folder

---
+++ src/pages/MarketingPlan.jsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import React from "react";
+import NavBar from "../components/Navbar";
+
+const MarketingPlan = () => {
+	return (
+		<div className={classNames(`bg-white duration-500`)}>
+			<p>Test</p>
+			<NavBar />
+		</div>
+	);
+};
+
+export default MarketingPlan;
