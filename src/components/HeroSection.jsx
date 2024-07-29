154f99ac4a2155a290f417fb63871285cf9bbbbb
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-29 17:48:00 -0400

    - Change Hero Image styling and hero section text content

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,9 +7,9 @@ const HeroSection = ({ scrollToForm }) => {
 				<div className="flex w-full ">
 					<div className="mx-auto">
 						<div className="-mt-2 flex flex-col items-center">
-							<img src={ProfileIMG} alt="image of joão" className="inline-block bg-white bg-[#cddff1] p-4  w-80 rounded-full" />
+							<img src={ProfileIMG} alt="image of joão" className="inline-block bg-[#cddff1] p-4  w-80 rounded-full" />
 							<div className="mt-12">
-								<h2 className="text-32 font-bold monoSlab text-center">The Serious Face of Marketing</h2>
+								<h2 className="text-32 font-bold monoSlab text-center">The Face of Marketing</h2>
 								<p className="mt-6 mx-20 text-24 text-center font-bold monoSlab">
 									Creative & strategic marketing to
 									<span className="p-2 ml-2 text-[#3078c0] bg-[#cddff1]">drive growth,</span>
