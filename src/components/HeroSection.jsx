5c39366ea83e3b3bf3ca9999d9cc963683164637
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-07-29 14:43:12 -0400

    - Change layout styling, image styling and text content

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,17 @@ import ProfileIMG from "/joaoHeadshot.jpg";
 const HeroSection = ({ scrollToForm }) => {
 	return (
 		<>
-			<section className="mx-4">
+			<section className="pt-20 mx-4">
 				<div className="flex w-full ">
 					<div className="mx-auto">
-						<div className="mt-2 flex flex-col items-center">
-							<img src={ProfileIMG} alt="image of joão" className="inline-block bg-white w-60 rounded-full" />
+						<div className="-mt-2 flex flex-col items-center">
+							<img src={ProfileIMG} alt="image of joão" className="inline-block bg-white bg-[#cddff1] p-4  w-80 rounded-full" />
 							<div className="mt-12">
-								<h2 className="text-32 font-bold monoSlab text-center"> An Aspiring Marketing Manager & Marketing Enthusiast </h2>
+								<h2 className="text-32 font-bold monoSlab text-center">The Serious Face of Marketing</h2>
 								<p className="mt-6 mx-20 text-24 text-center font-bold monoSlab">
-									Marketing strategies
-									<span className="p-2 ml-2 text-[#3078c0] bg-[#cddff1]">driving growth,</span>
-									<span className="text-[#3078c0] ml-2 p-2 bg-[#cddff1]">boosting visibility,</span> and <span className="text-[#3078c0] inline-block my-2 p-2 bg-[#cddff1]"> maximizing ROI</span> with Creative & Strategic Marketing for Your Business.
+									Creative & strategic marketing to
+									<span className="p-2 ml-2 text-[#3078c0] bg-[#cddff1]">drive growth,</span>
+									<span className="text-[#3078c0] ml-2 p-2 bg-[#cddff1]">boost visibility,</span> and <span className="text-[#3078c0] inline-block my-2 p-2 bg-[#cddff1]"> maximize ROI</span> for Your Business.
 								</p>
 							</div>
 							<div className="mt-10 flex gap-4 items-center text-center lg:text-left">
