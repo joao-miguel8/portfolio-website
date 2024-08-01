a178176a3f49d6809b945259fb5f2c2e7022bf41
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-08-01 10:55:18 -0400

    - Add Certifications.tsx and changed styling of About section and Added images/logos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,23 @@
+import Certifications from "./Certifications";
+import IphoneIMG from "../assets/iphone-img.avif";
+import EmailIMG from "../assets/email-img.avif";
+import SeoIMG from "../assets/seo-img.avif";
+import WebsiteIMG from "../assets/website-img.avif";
+import GoogleAdsIMG from "../assets/googleads-img.avif";
+
 const About = () => {
 	const coreServices = [
-		{ title: "Pay-Per-Click:PPC", description: "Looking to reach your ideal audience and maximize your ROI? Targeted PPC campaigns ensure your ads are seen by the right people at the right time." },
-		{ title: "Search Engine Optimization:SEO", description: "Need to boost your online visibility? Having a tailored SEO strategy can help your website rank higher, making it easier for customers to find you." },
-		{ title: "Paid Social Ads", description: "Looking to engage with your audience on social media? My eye-catching paid social ads connect you with your target audience and drive conversions." },
+		{ title: "SEM/PPC", img: GoogleAdsIMG },
+		{ title: "SEO", img: SeoIMG },
+		{ title: "Paid Social Ads", img: IphoneIMG },
+		{ title: "Website Development", img: WebsiteIMG },
+		{ title: "Email Marketing", img: EmailIMG },
 	];
 
 	return (
 		<>
 			<div className="mt-40 custom-shape-divider-top-1722475952 bg-[#3078c0]">
+				{/* front background design */}
 				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 					<path fill="#1d5fa1" d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
 					<path
@@ -17,27 +27,33 @@ const About = () => {
 						class="shape-fill"></path>
 					<path fill="#1d5fa1" d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
 				</svg>
-
 				<section id="about" className="mx-14 mt-20 min-h-[200px]">
-					<h2 className="text-34 monoSlab font-semibold text-white">Hi, I'm João!</h2>
-					<p className="mt-4 text-18 leading-8 montserrat text-white">
-						My journey began in the world of software development, initially fueled by a passion for game development. As I went deeper, I've learned web/software development in my free time and gained some freelance work experience.
+					<h2 className="text-center text-34 monoSlab font-semibold text-white">👋 Hi, I'm João!</h2>
+					<p className="text-center  mt-4 text-18 leading-8 montserrat text-white">
+						Before diving into digital marketing, I worked within the software development space. I obtained a freelance client and worked with them to build a website from scratch using React, but soon realized that a website alone wouldn't enhance the client's online presence. This led
+						me to explore digital marketing extensively.
 					</p>
-					<p className="mt-4 text-18 leading-8 montserrat text-white">
-						It wasn't long before I discovered the dynamic field of digital marketing. I realized that even the most brilliant website or SaaS product can't reach its full potential without effective marketing. This led me down the path of all things digital marketing.
+					<p className="text-center mt-4 text-18 leading-8 montserrat text-white">
+						Since then, I have optimized content for SEO, conducted SEO audits, managed Google Ads campaigns, performed keyword research, developed bid strategies, utilized analytics to understand KPIs, optimized landing pages, and conducted competitor analysis. I have also worked within
+						the facebook & bing ads interface and have used EmailOctopus to utilize email marketing and paid social media marketing.
 					</p>
-					<h3 className="mt-20 text-24 font-semibold uppercase">Core Services</h3>
-					<ul className="mt-4 flex flex-col gap-4 md:flex-row">
-						{coreServices.map(service => {
-							return (
-								<li key={service.title}>
-									<h5 className="text-18 font-semibold montserrat">{service.title}</h5>
-									<p className="montserrat">{service.description}</p>
-								</li>
-							);
-						})}
-					</ul>
+					<Certifications />
+					<div className="mt-20 mx-auto">
+						<h3 className="text-center text-24 font-semibold uppercase monoSlab text-white">Core Services</h3>
+						<div className="mt-4 flex flex-wrap justify-center">
+							{coreServices.map(service => {
+								return (
+									<div className={`group relative flex items-center justify-center bg-cover bg-center bg-no-repeat w-60 h-80 bg-black border-[1px]`} style={{ backgroundImage: `url(${service.img})` }}>
+										{/* overlay */}
+										<div className="absolute bottom-0 w-full h-full opacity-50 group-hover:opacity-0 bg-black duration-300"></div>
+										<h5 className="w-full text-center z-20 text-26 uppercase font-semibold montserrat text-white group-hover:bg-black opacity-75 duration-300">{service.title}</h5>
+									</div>
+								);
+							})}
+						</div>
+					</div>
 				</section>
+				{/* end background design */}
 				<div className="custom-shape-divider-bottom-1722476847 pt-20">
 					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 						<path fill="#1d5fa1" d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z" class="shape-fill"></path>
---
+++ src/components/Certifications.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GoogleLogo from "../assets/Google__G__logo.svg";
+import FacebookLogo from "../assets/facebook_logo.svg";
+import BingLogo from "../assets/Bing_Logo.svg";
+import HubSpotLogo from "../assets/HubSpot_Logo.svg";
+
+function Certifications() {
+	const certificationsList = [
+		{ logo: GoogleLogo, text: "Google Certified", size: "w-14" },
+		{ logo: FacebookLogo, text: "Facebook Certified", size: "w-14" },
+		{ logo: BingLogo, text: "Bing Certified", size: "w-16" },
+		{ logo: HubSpotLogo, text: "Hubspot Certified", size: "w-18" },
+	];
+	return (
+		<div className="mt-28">
+			<h3 className="uppercase text-center monoSlab font-semibold text-24 text-white">Certifications</h3>
+			{/* Certifications list */}
+			<div className="flex flex-wrap gap-10 justify-center">
+				{certificationsList.map(item => {
+					return (
+						<div key={item.text} className="mt-4 w-fit">
+							<span className="text-16 text-white montserrat font-semibold">{item.text}</span>
+							<img className={`mt-2 ${item.size} m-auto`} src={item.logo} alt={item.text + "image"} />
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
+
+export default Certifications;
