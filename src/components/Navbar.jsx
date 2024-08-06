02b9cad845a317ca4426846bd1665a4538ac078d
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-08-06 15:42:26 -0400

    - update icon imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
+import closeIcon from "../assets/close-icon.svg";
+import menuIcon from "../assets/menu-icon.svg";
 import "../App.css";
 
 const NavBar = () => {
@@ -17,7 +19,7 @@ const NavBar = () => {
 		<>
 			<div className={classNames(openNavMenu === true ? "translate-x-0" : "translate-x-full", "p-4 fixed top-0 right-0 z-50 duration-300 w-[320px] h-screen bg-white [box-shadow:rgba(0,_0,_0,_0.05)_0px_6px_24px_0px,_rgba(0,_0,_0,_0.08)_0px_0px_0px_1px] ")}>
 				<button onClick={() => setOpenNavMenu(false)} className="absolute top-4 right-3">
-					<img className="w-8" src="../assets/close-icon.svg" alt="close navigation menu" />
+					<img className="w-8" src={closeIcon} alt="close navigation menu" />
 				</button>
 				<nav className="ml-4 mt-20 mx-auto">
 					<ul className="flex flex-col gap-10">
@@ -43,7 +45,7 @@ const NavBar = () => {
 			<header className="sticky top-0 z-40 px-2 py-4 flex items-center border-b-2 border-gray-150 bg-white">
 				<img src="src/assets/favicon.png" alt="" className="w-10" />
 				<button onClick={() => setOpenNavMenu(true)} className="absolute right-4">
-					<img className="w-8" src="./src/assets/menu-icon.svg" alt="open navigation menu" />
+					<img className="w-8" src={menuIcon} alt="open navigation menu" />
 				</button>
 			</header>
 		</>
